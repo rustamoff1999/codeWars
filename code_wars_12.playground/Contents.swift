f0df3839e704e08390f25fac2d69e38806fc6cc0
fuadrustamov999 <rustamoff1999@gmail.com>
2020-07-08 09:16:08 +0400

    Two Sum

---
+++ code_wars_12.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+//:**Two Sum**
+
+//:*`DESCRIPTION`: Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a array like so: [index1, index2]. For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted. The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).*
+
+// FOR EXAMPLE: twoSum ([1, 2, 3] ,4) === [0, 2]
+class Solution {
+  static func twosum(numbers: [Double], target: Double) -> [Int] {
+    let n = numbers.count
+    var resultArray = [Int]()
+    for j in 0...n-1 {
+        for i in j+1...n-1 {
+            if numbers[i] + numbers[j] == target {resultArray = [j,i]}
+        }
+    }
+    return resultArray
+  }
+}
