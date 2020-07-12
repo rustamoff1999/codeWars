7feeb36a4700145c6569f650044ca58790711dbc
fuadrustamov999 <rustamoff1999@gmail.com>
2020-07-12 15:02:51 +0400

    A disguised sequence(Got 6 kyu in codewars)

--- code_wars_14.playground/Contents.swift
+++ code_wars_14.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-//:**A disguised sequence**
+//:**A disguised sequence** (Got 6 kyu in codewars!)
 /**
  Given u0 = 1, u1 = 2 and the relation 6u_n*u_(n+1)-5u_n*u_(n+2)+u_(n+1)*u_(n+2) = 0 calculate un for any integer n >= 0.
  - `Algorithm`:  is to find which sequence is hidden behind un.
